e9003f15fd3dd8a5ed4331716791792931ddbd14
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-26 10:43:31 +0530

    adding licence program.

---
+++ licence/js/main.js
@@ -0,0 +1,18 @@
+var age=parseInt(prompt("enter the age"));
+var test="ok";
+if(age>=18)
+{
+if(test === "ok")
+{
+	console.log(" you have licence");
+}
+else
+{
+	console.log("you have not licence");
+}
+}
+
+else
+{
+	console.log("you are young");
+}
